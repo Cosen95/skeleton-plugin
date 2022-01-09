267f4786990fc47545490ff490a5a56dcab296b0
Cosen95 <1263215592@qq.com>
2022-01-09 22:57:57 +0800

    feat: 基础结构

---
+++ skeleton/SkeletonPlugin.js
@@ -0,0 +1,7 @@
+const PLUGIN_NAME = "SkeletonPlugin"; // 插件名称
+
+class SkeletonPlugin {
+  apply(compiler) {}
+}
+
+module.exports = SkeletonPlugin;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <img src="https://gitee.com/fengshuan/assets/raw/master/2022-1-9/1641716138626-%E5%BE%AE%E4%BF%A1%E5%88%86%E4%BA%AB02.jpeg" />
+    <button>普通按钮</button>
+  </div>,
+  document.getElementById("root")
+);
